5b0116c47ce7d0cb3f194709cf28bd4dc3e7a85b
sathvikt23 <141807646+sathvikt23@users.noreply.github.com>
2024-06-09 02:51:22 +0530

    upgrade

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import chunks as ck
 import embeddings as eb
 import similarity as sm
 import LoadLLM as llm
-"""
+
 ET= web.ExtractText()
 pages_and_texts=ET.urlDownloadLink(url="https://pressbooks.oer.hawaii.edu/humannutrition2/open/download?type=pdf")
 print(pages_and_texts)
@@ -11,9 +11,9 @@ CK= ck.ChunksConversion()
 pages_and_chunks_over_min_token_len=CK.Convert(pages_and_texts)
 EB=eb.genrateEmbeddings("cuda")
 EB.get(pages_and_chunks_over_min_token_len[0])
-embeddings_df_save_path=EB.saveToCsv()"""
+embeddings_df_save_path=EB.saveToCsv()
 SM=sm.search("text_chunks_and_embeddings_df.csv","cuda")
-LLM=llm.gemma("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")
+LLM=llm.gemma("hf_api")
 def Generate(query):
   scores_index_pages=SM.getEmbeddings(query)
   LLM.askGemma(query,scores_index_pages[2],scores_index_pages[0],scores_index_pages[1])
@@ -23,4 +23,3 @@ Generate("Who is Narendra Modi ")
 print("==============================================================")
 Generate("Using Eyes of Discernment")
 
-#upload to git 
